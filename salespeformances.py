cf1ecd9840b7e046855f12f9c9f3f21238439eac
FokLZ <137991906+FokLZ@users.noreply.github.com>
2023-11-28 10:16:32 +0800

    Update salespeformances.py

--- salespeformances.py
+++ salespeformances.py
@@ -1,9 +1,5 @@
 import numpy as np 
 import pandas as pd 
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
-from sklearn.impute import SimpleImputer
-from sklearn.metrics import mean_squared_error
 import matplotlib.pyplot as plt
 
 # Input data files are available in the read-only "../input/" directory
